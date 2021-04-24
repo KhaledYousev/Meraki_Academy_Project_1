10e99a5b0398a7bc68c8af2d984ad355a2d6f2ab
KhaledYousev <mr.khaledyusuf@gmail.com>
2021-04-24 19:29:28 +0300

    Question html,CSS,JS,graphic edits & enhancments

--- questions.js
+++ questions.js
@@ -1,2 +1,104 @@
+let questions = [
+  {
+    question: "What is the /Mafjoo3a/ element in HTML Display?",
+    choice1: "Block Element",
+    choice2: "Array ELement",
+    choice3: "Inline ELement",
+    choice4: "<Tags> ELement",
+    answer: 1
+  },
+  {
+    question:
+      "What date Cohort 2 had been started?",
+    choice1: "26/4/2021",
+    choice2: "27/4/2021",
+    choice3: "28/04/2021",
+    choice4: "29/04/2021",
+    answer: 3
+  },
+  {
+    question: " How do you write 'MoMo' in an alert box?",
+    choice1: "msgBox('MoMo');",
+    choice2: "alertBox('MoMo');",
+    choice3: "alert('MOMO');",
+    choice4: "nono of the above",
+    answer: 4
+  }
+];
 
 
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+startquestions = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("./end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startquestions();
